1a16c68ebfd3f2a047848859ef9b085473819934
Ciprian <48722279+Ciprian-A@users.noreply.github.com>
2021-03-31 13:51:14 +0100

    added hangman

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,6 @@
 import calc from '../assets/calc.png'
 import beer from '../assets/beer.jpg'
-import hangman from '../assets/hangman.jpeg'
+import hangman from '../assets/hangman.png'
 import bbad from '../assets/breaking-bad.jpg'
 import converter from '../assets/converter.png'
 import morse from '../assets/morse.jpg'
@@ -17,21 +17,21 @@ const projectsData = [
   },
   {
     id: 2,
-    name: 'Punk API',
-    imageSrc: beer, 
-    description: 'Coming soon',
-    gitLink: '#',
-    liveSite: '#'
+    name: 'Morse Code Translator',
+    imageSrc: morse,
+    description: 'Morse Code Translator app made with Vanilla Javascript, HTML and SCSS. It allows you to convert english to morse code and morse code to english',
+    gitLink: 'https://github.com/Ciprian-A/morse-translator',
+    liveSite: 'https://morse.ciprian-andrei-wd.co.uk'
 
   },
   {
     id: 3,
     name: 'Hangman Game',
     imageSrc: hangman, 
-    description: 'Coming soon',
-    gitLink: '#',
-    liveSite: '#'
-
+    description: 'Hangman Game made with HTML, SCSS and Vanilla Javascript',
+    gitLink: 'https://github.com/Ciprian-A/Hangman',
+    liveSite: 'https://hangman.ciprian-andrei-wd.co.uk'
+    
   },
   {
     id: 4,
@@ -40,7 +40,7 @@ const projectsData = [
     description: 'A React app that allows you to search for the main characters in the famous tv-series.',
     gitLink: 'https://github.com/Ciprian-A/breaking-bad-cast',
     liveSite: 'https://breaking-bad-cast.ciprian-andrei-wd.co.uk'
-
+    
   },
   {
     id: 5,
@@ -49,18 +49,18 @@ const projectsData = [
     description: 'Currency convertor made with React.Js, SCSS which allows you to check the latest currency rates',
     gitLink: 'https://github.com/Ciprian-A/currency-converter',
     liveSite: 'https://currency.ciprian-andrei-wd.co.uk'
-
+    
   },
   {
     id: 6,
-    name: 'Morse Code Translator',
-    imageSrc: morse,
-    description: 'Morse Code Translator app made with Vanilla Javascript, HTML and SCSS.',
-    gitLink: 'https://github.com/Ciprian-A/morse-translator',
-    liveSite: 'https://morse.ciprian-andrei-wd.co.uk'
-
-  }
-
+    name: 'Punk API',
+    imageSrc: beer, 
+    description: 'Coming soon',
+    gitLink: '#',
+    liveSite: '#'
+  
+  },
+  
 ]
 
 export default projectsData
