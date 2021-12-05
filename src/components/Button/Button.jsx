d46407b5bfa3bee64c8d59d88314fea0da886a14
21ciprian <2021ciprianoffice@gmail.com>
2021-12-05 22:31:32 +0000

    Changed the colors of the project cards and the header animation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import styles from './Button.module.scss';
- 
 
-
-const Button = ({name, link}) => {
+const Button = ({ name, link }) => {
   return (
     <div>
       <button className={styles.button}>
-        <a href={link} target='_blank' rel= 'noreferrer' noopener>{name}</a>
+        <a href={link} target='_blank' rel='noreferrer' noopener>
+          {name}
+        </a>
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
